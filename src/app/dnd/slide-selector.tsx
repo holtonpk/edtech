bf29ff9cb7b4c47cd59af77630247641ae57d11d
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-09-27 23:14:25 -0500

    lint error

--- src/app/dnd/slide-selector.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import {Layout, Pages} from "./pages2";
-
-const SlideSelector = () => {
-  return <Pages layout={Layout.Vertical} />;
-};
-
-export default SlideSelector;
