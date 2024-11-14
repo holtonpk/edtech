b4da807bac2d70f91b611453014a0a2f758a9615
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-11-14 16:00:51 -0600

    export moved

--- src/app/(auth)/(tool)/dashboard/page.tsx
+++ src/app/(auth)/(tool)/dashboard/page.tsx
@@ -108,13 +108,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-export const NavBar = () => {
-  return (
-    <div className="flex w-fit gap-4 poppins-bold text-muted-foreground">
-      <a>About</a>
-      <a>Learn</a>
-      <a>Share</a>
-    </div>
-  );
-};
